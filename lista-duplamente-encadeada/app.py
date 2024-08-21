c60f16fe7e61562af63753af1b97252747c86202
Judson <pequenojudson@gmail.com>
2024-08-21 20:48:35 -0300

    aplicando todas as funcionalidades

--- lista-duplamente-encadeada/app.py
+++ lista-duplamente-encadeada/app.py
@@ -1,10 +1,11 @@
 from teste import Produto, ListaDuplamenteEncadeada
 
 produto = Produto('Nub', '123', 15, 12)
-produto2 = Produto('jusu', '321', 45, 85)
+produto2 = Produto('furadeira eletrica 220w sigma poewr', '321', 45, 85)
 lista = ListaDuplamenteEncadeada()
 lista.adicionar(produto)
 lista.adicionar(produto2)
 lista.listar()
+lista.atualizar_estoque('321',8)
 lista.remover('123')
 lista.listar()
