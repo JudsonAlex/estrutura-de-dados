f22da896e5e05f9a0be8c7e176d8644eeaca6a0a
Judson <pequenojudson@gmail.com>
2024-08-26 16:44:29 -0300

    correção de portugues no codigo

--- lista-encadeada/app.py
+++ lista-encadeada/app.py
@@ -1,6 +1,6 @@
 from listaEncadeada import ListaDePacientes, Paciente
 
-judson = Paciente('Judson', 1, "Saldável")
+judson = Paciente('Judson', 1, "Saudável")
 david = Paciente('David', 2, "Nubando")
 
 lista = ListaDePacientes()
