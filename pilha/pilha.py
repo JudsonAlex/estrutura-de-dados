4f68a91c8a0b0d8c4e267d18718660dc15ab273f
Judson <pequenojudson@gmail.com>
2024-08-27 16:18:38 -0300

    criando a base da estrutura de pilha

---
+++ pilha/pilha.py
@@ -0,0 +1,21 @@
+class Livro:
+    def __init__(self, titulo, paginas) -> None:
+        self.titulo = titulo
+        self.n_paginas = paginas
+
+
+class PilhaDeLivros:
+    def __init__(self) -> None:
+        pass
+
+    def adicionar(self, livro):
+        pass
+
+    def remover(self):
+        pass
+
+    def exibirTopo(self):
+        pass
+
+    def mostrarTodos(self):
+        pass
